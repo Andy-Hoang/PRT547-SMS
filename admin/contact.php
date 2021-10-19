182e76b2fea55ff408a4bd746f517aeff01be17a
AndyDD_ThangHoang <thangvnhoang@gmail.com>
2021-10-19 09:34:54 +0930

    upload to local Git

---
+++ admin/contact.php
@@ -0,0 +1,20 @@
+<?php
+require_once('../include/header.php');
+?>
+
+<nav class="teal">
+  <div class="container">
+    <div class="nav-wrapper">
+      <a href="" class="brand-logo center">DIVINE LIGHT HIGHER SECONDARY SCHOOL</a>
+      <a href="" class="sidenav-trigger show-on-large" data-target="slide-out"><i class="material-icons">menu</i></a>
+    </div>        
+  </div>
+</nav>
+
+<?php
+require_once('../include/sidenav.php');
+?>
+
+<?php
+require_once('../include/footer.php');
+?>
---
+++ include/dbcon.php
@@ -0,0 +1,8 @@
+<?php
+$dbname = "prt547sms";
+$dbusername ="root";
+$dbpassword = "";
+$dbhost = "localhost";
+
+$con = mysqli_connect($dbhost,$dbusername,$dbpassword,$dbname);
+?>
